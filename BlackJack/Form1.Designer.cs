f93c058050b281e6182d9365d3b95414c28160a1
Subhabrata Mukherjee <bc110@lab.cpcc.edu>
2018-03-28 11:07:19 -0400

    first commit

---
+++ BlackJack/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace BlackJack
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDeal = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnDeal
+            // 
+            this.btnDeal.Location = new System.Drawing.Point(1248, 13);
+            this.btnDeal.Name = "btnDeal";
+            this.btnDeal.Size = new System.Drawing.Size(59, 42);
+            this.btnDeal.TabIndex = 10;
+            this.btnDeal.Text = "Deal";
+            this.btnDeal.UseVisualStyleBackColor = true;
+            this.btnDeal.Click += new System.EventHandler(this.btnDeal_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1248, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(59, 42);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1344, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnDeal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnDeal;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ BlackJack/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlackJack
+{
+    public partial class Form1 : Form
+    {
+        public int playercard = 0;
+        Random rnd1 = new Random();
+        List<int> playerdeck = new List<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDeal_Click(object sender, EventArgs e)
+        {
+            DisplayImage(GenerateCard());
+        }
+
+        private void DisplayImage(string file)
+        {
+            PictureBox picture = new PictureBox
+            {
+                Name = "pictureBox",
+                Size = new Size(125, 200),
+                Location = new Point(playercard * 140 + 10, 5),
+                Image = Image.FromFile(file),
+                SizeMode = PictureBoxSizeMode.AutoSize
+            };
+
+            Controls.Add(picture);
+            playercard++;
+        }
+
+        private bool CheckCard(int cardno)
+        {
+            bool alreadyExists = playerdeck.Any(x => x == cardno);
+            return alreadyExists;
+        }
+
+        private string GenerateCard()
+        {            
+            string cardimage = ""; 
+            int cardno = 0;
+            cardno = rnd1.Next(1, 14);
+
+            if (playerdeck.Count != 13)
+            {
+                while (CheckCard(cardno))
+                    cardno = rnd1.Next(1, 14);
+            }
+
+            switch (cardno)
+            {
+                case 1:
+                    cardimage = "C:\\Users\\bc110\\source\\repos\\BlackJack\\BlackJack\\images\\A.PNG";
+                    break;
+                case 2:
+                    cardimage = "C:\\Users\\bc110\\source\\repos\\BlackJack\\BlackJack\\images\\2.PNG";
+                    break;
+                case 3:
+                    cardimage = "C:\\Users\\bc110\\source\\repos\\BlackJack\\BlackJack\\images\\3.PNG";
+                    break;
+                case 4:
+                    cardimage = "C:\\Users\\bc110\\source\\repos\\BlackJack\\BlackJack\\images\\4.PNG";
+                    break;
+                case 5:
+                    cardimage = "C:\\Users\\bc110\\source\\repos\\BlackJack\\BlackJack\\images\\5.PNG";
+                    break;
+                case 6:
+                    cardimage = "C:\\Users\\bc110\\source\\repos\\BlackJack\\BlackJack\\images\\6.PNG";
+                    break;
+                case 7:
+                    cardimage = "C:\\Users\\bc110\\source\\repos\\BlackJack\\BlackJack\\images\\7.PNG";
+                    break;
+                case 8:
+                    cardimage = "C:\\Users\\bc110\\source\\repos\\BlackJack\\BlackJack\\images\\8.PNG";
+                    break;
+                case 9:
+                    cardimage = "C:\\Users\\bc110\\source\\repos\\BlackJack\\BlackJack\\images\\9.PNG";
+                    break;
+                case 10:
+                    cardimage = "C:\\Users\\bc110\\source\\repos\\BlackJack\\BlackJack\\images\\10.PNG";
+                    break;
+                case 11:
+                    cardimage = "C:\\Users\\bc110\\source\\repos\\BlackJack\\BlackJack\\images\\J.PNG";
+                    break;
+                case 12:
+                    cardimage = "C:\\Users\\bc110\\source\\repos\\BlackJack\\BlackJack\\images\\Q.PNG";
+                    break;
+                case 13:
+                    cardimage = "C:\\Users\\bc110\\source\\repos\\BlackJack\\BlackJack\\images\\K.PNG";
+                    break;
+            }
+            playerdeck.Add(cardno);
+
+            return cardimage;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            playercard = 0;
+            foreach (Control item in Controls.OfType<PictureBox>())
+            {
+                if (item.Name.Contains("pictureBox"))
+                    Controls.Remove(item);
+            }
+
+        }
+    }
+}
